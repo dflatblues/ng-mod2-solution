82656471d8569a349d8c498690004b87a212f4e8
Juan Sánchez <jsanchez.gr@gmail.com>
2016-09-24 13:11:20 +0200

    Loading controllers

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+(function () {
+    'use strict';
 
+    var app = angular.module('ShoppingListCheckOff', []);
+
+    app.controller('ToBuyShoppingController', function ($scope) {
+
+    });
+
+    app.controller('AlreadyBoughtShoppingController', function ($scope) {
+
+    });
+
+})();
