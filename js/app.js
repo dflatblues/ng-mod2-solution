66d4cce0a58fab63d0ee4980ed338ec0b2dc1cdd
Juan Sánchez <jsanchez.gr@gmail.com>
2016-09-25 20:25:08 +0200

    Service and controllers working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
 (function () {
     'use strict';
 
-    var app = angular.module('ShoppingListCheckOff', []);
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyShoppingController', ToBuyShoppingController)
+        .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-    app.controller('ToBuyShoppingController', function ($scope) {
+    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+        this.items = ShoppingListCheckOffService.getCart();
+    }
 
-    });
+    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyShoppingController(ShoppingListCheckOffService) {
+        this.items = ShoppingListCheckOffService.getProducts();
 
-    app.controller('AlreadyBoughtShoppingController', function ($scope) {
+        this.buyItem = function (itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        }
+    }
 
-    });
+    function ShoppingListCheckOffService() {
+        var service  = this;
+        var cart     = [];
+        var products = [
+            { name: "cookies", quantity: 10 },
+            { name: "chips", quantity: 3 },
+            { name: "beer", quantity: 24 },
+            { name: "orange juice", quantity: 5 },
+            { name: "watermelon", quantity: 1 },
+            { name: "apples", quantity: 6 },
+            { name: "chocolate", quantity: 2 },
+            { name: "milk", quantity: 6 }
+        ];
+
+        service.buyItem = function (itemIndex) {
+            cart.push(products[itemIndex]);
+            products.splice(itemIndex, 1);
+        }
+
+        service.getCart = function () {
+            return cart;
+        }
+
+        service.getProducts = function () {
+            return products;
+        }
+    }
 
 })();
